64af91a5f18d2d9c4dd53b2e6562fd7136910551
Ibrahim Tharwat <161630969+itiswd@users.noreply.github.com>
2024-08-11 12:43:24 +0000

    Cubit Setup

--- lib/features/iot_control/presention/manger/iot_control/iot_control_cubit.dart
+++ lib/features/iot_control/presention/manger/iot_control/iot_control_cubit.dart
@@ -1,5 +1,4 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -7,42 +6,34 @@ part 'iot_control_state.dart';
 
 class IotControlCubit extends Cubit<IotControlState> {
   IotControlCubit() : super(IotControlInitial());
-   final Stream<QuerySnapshot> usersStream =FirebaseFirestore.instance.collection('iot_control').snapshots();
-    bool redLed = false;
+  final Stream<QuerySnapshot> usersStream =
+      FirebaseFirestore.instance.collection('iot_control').snapshots();
+  bool redLed = false;
   bool greenLed = false;
   bool fan = false;
 
-  //   void updateSwitchValue(bool value, String field,AsyncSnapshot<QuerySnapshot<Object?>> snapshot) async {
-  //     emit(IotControlLoading());
-  //       final document0 = snapshot.data!.docs[0];
-  //       final document1 = snapshot.data!.docs[1];
-  //       fan = document0['fan'];
-  //       redLed = document0['red'];
-  //       greenLed = document0['green'];
-  //   try {
-  //     await FirebaseFirestore.instance
-  //         .collection('iot_control')
-  //         .doc(documentId)
-  //         .update({
-  //       field: value,
-  //     });
-      
-  //       switch (field) {
-  //         case 'red':
-  //           redLed = value;
-  //           break;
-  //         case 'green':
-  //           greenLed = value;
-  //           break;
-  //         case 'fan':
-  //           fan = value;
-  //           break;
-  //       }
-  //        emit(IotControlSuccess());
-     
-  //   } catch (e) {
-  //     emit(IotControlIFailure ());
-  //     print('Failed to update data: $e');
-  //   }
-  // }
+  //Update Value
+  void updateSwitchValue(bool value, String documentId, String field) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('iot_control')
+          .doc(documentId)
+          .update({
+        field: value,
+      });
+      switch (field) {
+        case 'red':
+          redLed = value;
+          break;
+        case 'green':
+          greenLed = value;
+          break;
+        case 'fan':
+          fan = value;
+          break;
+      }
+    } catch (e) {
+      debugPrint('Failed to update data: $e');
+    }
+  }
 }
