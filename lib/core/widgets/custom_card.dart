3b4e777af37e9e8d840f4355cfeb77098aefd2cf
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-11 08:28:21 +0300

    create stream database

--- lib/core/widgets/custom_card.dart
+++ lib/core/widgets/custom_card.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-class CustomCard extends StatefulWidget {
+
+class CustomCard extends StatelessWidget {
   const CustomCard({
     Key? key,
     required this.iconOn,
     required this.iconOff,
     required this.text,
-    required this.onSwitchChanged, required this.color,
+    required this.onToggle,
+    required this.color,
+    required this.value,
   }) : super(key: key);
+
   final IconData iconOn;
   final IconData iconOff;
   final String text;
-  final Function onSwitchChanged;
-  final Color color ;
-  @override
-  State<CustomCard> createState() => _CustomCardState();
-}
-
-class _CustomCardState extends State<CustomCard> {
-  bool _isSwitchOn = false;
+  final VoidCallback onToggle;
+  final bool value;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
@@ -33,35 +33,45 @@ class _CustomCardState extends State<CustomCard> {
           elevation: 50,
           shadowColor: const Color(0xFF373C59),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            padding: const EdgeInsets.all(10),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon( _isSwitchOn ? widget.iconOn : widget.iconOff,
-              color: _isSwitchOn ? widget.color : Colors.grey,
-              size: 50,),
-                const Spacer(),
-                Text(
-                  widget.text,
-                  style: const TextStyle(fontSize: 20),
-                ),
-                const Spacer(),
-                Switch(
-                  value: _isSwitchOn,
-                  onChanged: (value) {
-                    setState(() {
-                      _isSwitchOn = value;
-                      widget.onSwitchChanged(value);
-                    });
-                  },
-                  activeColor: const Color(0xFF201C32),
-                  inactiveThumbColor: Colors.grey,
-                  inactiveTrackColor: Colors.grey.withOpacity(0.5),
-                ),
+                _buildIcon(),
+                const SizedBox(height: 20), // Spacer replacement
+                _buildText(),
+                const SizedBox(height: 20), // Spacer replacement
+                _buildSwitch(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Icon _buildIcon() {
+    return Icon(
+      value ? iconOn : iconOff,
+      color: value ? color : Colors.grey,
+      size: 50,
+    );
+  }
+
+  Text _buildText() {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 20),
+    );
+  }
+
+  Switch _buildSwitch() {
+    return Switch(
+      value: value,
+      onChanged: (newValue) => onToggle(),
+      activeColor: const Color(0xFF201C32),
+      inactiveThumbColor: Colors.grey,
+      inactiveTrackColor: Colors.grey.withOpacity(0.5),
     );
   }
 }
--- lib/features/iot_control/presention/views/widgets/iot_control_view_body.dart
+++ lib/features/iot_control/presention/views/widgets/iot_control_view_body.dart
@@ -1,64 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import '../../../../../core/widgets/custom_card.dart';
-
-
-
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class IotControlViewBody extends StatefulWidget {
-  const IotControlViewBody({super.key});
+  const IotControlViewBody({Key? key}) : super(key: key);
 
   @override
   State<IotControlViewBody> createState() => _IotControlViewBodyState();
 }
 
 class _IotControlViewBodyState extends State<IotControlViewBody> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('leds').snapshots();
+  bool _switchValue = false;
+  
+
+  void _updateSwitchValue(QuerySnapshot snapshot)  {
+  
+    _switchValue = snapshot.docs.isNotEmpty ? snapshot.docs[0]['red'] : false;
+    print(_switchValue);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Row(
-          children: [
-            CustomCard(
-              iconOn: Icons.lightbulb,
-              iconOff: Icons.lightbulb,
-              color: Colors.red,
-              text: 'red led',
-              onSwitchChanged: (bool value) {
-                // Do something based on the switch value
-                print('Switch value changed: $value');
-              },
-            ),
-            CustomCard(
-              iconOn: Icons.lightbulb,
-              iconOff: Icons.lightbulb,
-              color: Colors.green,
-              text: 'green led',
-              onSwitchChanged: (bool value) {
-                // Do something based on the switch value
-                print('Switch value changed: $value');
-              },
-            ),
-        
-          ],
-        ),    CustomCard(
-              iconOn:  Icons.flip_camera_android_rounded,
-              iconOff: Icons.mode_fan_off,
-              color: Colors.blue,
-              text: 'fan',
-              onSwitchChanged: (bool value) {
-                // Do something based on the switch value
-                print('Switch value changed: $value');
-              },
-              
-            ),
-            SizedBox(
-              height: 300,
-            )
-         
-      ],
-    ));
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Center(child: Text('Something went wrong'));
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        // Update switch value based on the snapshot data
+        _updateSwitchValue(snapshot.data!);
+
+        return SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  CustomCard(
+                    iconOn: Icons.lightbulb,
+                    iconOff: Icons.lightbulb,
+                    color: Colors.red,
+                    text: 'Red LED',
+                    value: _switchValue,
+                    onToggle: () => _updateSwitchValue(snapshot.data!),
+                  ),
+                  CustomCard(
+                    iconOn: Icons.lightbulb,
+                    iconOff: Icons.lightbulb,
+                    color: Colors.green,
+                    text: 'green led',
+                    value: _switchValue,
+                    onToggle: () => _updateSwitchValue(snapshot.data!),
+                  ),
+                ],
+              ),
+              CustomCard(
+                  iconOn: Icons.flip_camera_android_rounded,
+                  iconOff: Icons.mode_fan_off,
+                  color: Colors.blue,
+                  text: 'fan',
+                  value: _switchValue,
+                  onToggle: () => _updateSwitchValue(snapshot.data!)),
+                  SizedBox(height: 350),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
+
+
+
+
+
+
+         
+
+
+  // FirebaseFirestore.instance
+  //       .collection('leds')
+  //       .doc("SQpudz4SuueHbzYlQgmq")
+  //       .update({
+  //     'red': !_switchValue, // Toggle the current value
+  //   });
